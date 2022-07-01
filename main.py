37c24ced9e7cb88e7e3f43814479e806cbe6df81
Robin_Dillen <Robin.Dillen@student.uantwerpen.be>
2022-07-01 13:56:37 +0200

    fixed issue with inspect module

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+import builtins
 import inspect
 from functools import wraps
+
+from forbiddenfruit import cursed, curse
+
+
+def get_unbound_vars(func) -> set[str]:
+    """
+    this is a snippet from the 'inspect' module
+    :param func: a function
+    :return: all the unbound variables
+    """
+    code = func.__code__
+    global_ns = func.__globals__
+    builtin_ns = global_ns.get("__builtins__", builtins.__dict__)
+    if inspect.ismodule(builtin_ns):
+        builtin_ns = builtin_ns.__dict__
+    unbound_names: set = set()
+
+    for name in code.co_names:
+        if name in ("None", "True", "False") or name in global_ns or name in builtin_ns:
+            # Because these used to be builtins instead of keywords, they
+            # may still show up as name references. We ignore them.
+            continue
+        unbound_names.add(name)
+    return unbound_names
 
 
 class CClass(type):
     def __new__(mcs, *args, **kwargs):
         if '__init__' not in args[2]:
             raise Exception("Must implement an __init__ function!")
-        vars_ = inspect.getclosurevars(args[2]['__init__']).unbound
+        vars_ = get_unbound_vars(args[2]['__init__']) - (set(args[2].keys()) | {args[0], })
         args[2]["__slots__"] = vars_
 
         def __getattribute__(self, name):
@@ -76,9 +101,12 @@ class Foo(metaclass=CClass):
 
 class Bar(Foo):
     def __init__(self):
-        self.d = 10
+        self.d = "hello"
         super(Bar, self).__init__()
 
+bar = Bar()
+bar.pub()
+bar.prot()
 
 foo = Foo()
 foo.pub()
